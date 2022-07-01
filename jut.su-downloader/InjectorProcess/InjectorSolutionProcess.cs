a1fe6452fb45ecdcfa887864a35cc2d01ce3d82a
telikey <49363725+telikey@users.noreply.github.com>
2022-07-02 01:44:47 +0300

    Update InjectorSolutionProcess.cs

--- jut.su-downloader/InjectorProcess/InjectorSolutionProcess.cs
+++ jut.su-downloader/InjectorProcess/InjectorSolutionProcess.cs
@@ -20,8 +20,8 @@ namespace jut.su_downloader.InjectorProcess
             Injector.Add<AllCommands, AllCommands>(null, true);
             Injector.Add<ICommandLogic<ICommand>, CommandLogic<ICommand>>(new object[]
             {
-                Injector.GetObject<AllCommands>(),
-                "Command"
+                "Command",
+                "some"
             }, true);
             Injector.Add<IDownloaderLogic, Jut_su_Logic>(null,true);
             Injector.Add<MainWindowVM, MainWindowVM>(null, true);
